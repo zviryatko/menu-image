ab17c7ba66ed093a992a32cfe6597685c93c7b5c
zviryatko <zviryatko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-06-02 15:18:07 +0000

    Update tests run configuration.
    git-svn-id: http://plugins.svn.wordpress.org/menu-image/trunk@1885985 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/setup.sh
+++ tests/setup.sh
@@ -5,13 +5,25 @@ export $(egrep -v '^#' .env | xargs)
 DIR=${PWD}
 WP=${DIR}/vendor/bin/wp
 cd ${WP_ROOT_FOLDER}
-mysql -u${DB_USER} -h${DB_HOST} -e "DROP DATABASE IF EXISTS ${DB_NAME}; CREATE DATABASE ${DB_NAME};"
 
+# Create database and drop previous one.
+mysql -u${DB_USER} $(if [ ! -z DB_PASSWORD ]; then echo "-p${DB_PASSWORD}"; fi)  -h${DB_HOST} -e "DROP DATABASE IF EXISTS ${DB_NAME}; CREATE DATABASE ${DB_NAME};"
+
+# Install and configure wordpress.
 ${WP} config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=${DB_HOST} --dbprefix=${TABLE_PREFIX}
 ${WP} core install --url=${WP_URL} --title=DevPress --admin_user=${ADMIN_USERNAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --skip-email
-sed -i "/define( 'DB_COLLATE', '' );/ a\if(!defined('DISABLE_WP_CRON')) define('DISABLE_WP_CRON', true);" wp-config.php
+
+# Add setting to disable cron.
+if ! grep -q DISABLE_WP_CRON wp-config.php; then
+    sed -i "/define( 'DB_COLLATE', '' );/ a\if(!defined('DISABLE_WP_CRON')) define('DISABLE_WP_CRON', true);" wp-config.php
+fi
+
+# Link plugin to wordpress plugins subfolder and activate it.
+rm -rf ${WP_ROOT_FOLDER}/wp-content/plugins/menu-image
 ln -s ${DIR} ${WP_ROOT_FOLDER}/wp-content/plugins/menu-image
 ${WP} plugin activate menu-image
+
+# Dump database for codeception.
 if [ -z "${DIR}/tests/_data/dump.sql" ]; then
     rm ${DIR}/tests/_data/dump.sql
 fi;
