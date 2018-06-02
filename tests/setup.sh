dc3959cae1c469e330e046bf1e6c5cb004eaba4b
zviryatko <zviryatko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-06-02 15:19:16 +0000

    Fix theme env configuration for travis.
    git-svn-id: http://plugins.svn.wordpress.org/menu-image/trunk@1885992 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/setup.sh
+++ tests/setup.sh
@@ -3,8 +3,7 @@
 # Export the vars in .env into your shell:
 export $(egrep -v '^#' .env | xargs)
 DIR=${PWD}
-WP=${DIR}/vendor/bin/wp
-cd ${WP_ROOT_FOLDER}
+WP="${DIR}/vendor/bin/wp --path=${WP_ROOT_FOLDER}"
 
 # Create database and drop previous one.
 mysql -u${DB_USER} $(if [ ! -z ${DB_PASSWORD} ]; then echo "-p${DB_PASSWORD}"; fi)  -h${DB_HOST} -e "DROP DATABASE IF EXISTS ${DB_NAME}; CREATE DATABASE ${DB_NAME};"
@@ -14,9 +13,14 @@ ${WP} config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbhost=${DB_HOST} 
 ${WP} core install --url=${WP_URL} --title=DevPress --admin_user=${ADMIN_USERNAME} --admin_password=${ADMIN_PASSWORD} --admin_email=${ADMIN_EMAIL} --skip-email
 
 # Add setting to disable cron.
-if ! grep -q DISABLE_WP_CRON wp-config.php; then
-    sed -i "/define( 'DB_COLLATE', '' );/ a\if(!defined('DISABLE_WP_CRON')) define('DISABLE_WP_CRON', true);" wp-config.php
+if ! grep -q DISABLE_WP_CRON ${WP_ROOT_FOLDER}/wp-config.php; then
+    sed -i "/define( 'DB_COLLATE', '' );/ a\if(!defined('DISABLE_WP_CRON')) define('DISABLE_WP_CRON', true);" ${WP_ROOT_FOLDER}/wp-config.php
 fi
+
+if [ ! -z ${WP_THEME} ]; then
+  ${WP} theme install $WP_THEME
+  ${WP} theme activate $WP_THEME
+fi;
 
 # Link plugin to wordpress plugins subfolder and activate it.
 rm -rf ${WP_ROOT_FOLDER}/wp-content/plugins/menu-image
